994aab72eaa6fe67377fe1326519fab889c0fc3c
jebuk97 <jebuk97@gmail.com>
2021-03-25 21:04:06 +0900

    홈화면 배열 처리

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,89 +1,114 @@
 import React from 'react';
-import { View, Button, TouchableOpacity, Text, StyleSheet, ScrollView } from 'react-native';
+import { render } from 'react-dom';
+import { View, Button, TouchableOpacity, Text, StyleSheet, ScrollView, Dimensions } from 'react-native';
 import { withOrientation } from 'react-navigation';
+var menus = [
+  {
+    id: 1,
+    img: '',
+    menu: '돈까스',
+    price:10000
+  },
+  {
+    id: 2,
+    img: '',
+    menu: '치즈돈까스',
+    price:12000
+  },
+  {
+    id: 3,
+    img: '',
+    menu: '매운돈까스',
+    price:12000
+  }
+]
 
-const Home = () => {
-  return (
-    <ScrollView stickyHeaderIndices={[1]} style={{flex: 1, backgroundColor: 'rgb(242, 242, 247)'}}>
-      <View>
-        <View style={styles.menuContainer}>
-          <View style={styles.menu}>
-                <Text>menu</Text>
-                
-          </View>
-          <View style={styles.menu}>
-                <Text>menu</Text>
-                
-          </View>
+var cart = [
+  {
+    menu: '돈까스',
+    qty: 2,
+    price: 10000
+  },
+  {
+    menu: '치즈돈까스',
+    qty: 2,
+    price: 12000
+  }
+]
+var cart1 = []
+var cart2 = []
+
+class Home extends React.Component{
+  printMenus(menu){
+    if(menu.id%2==0) {
+    return;
+    }
+    else{
+      return (
+        <View style={styles.menu}>
+        <View>
+          <Text>{menu.menu}</Text>
         </View>
-        <View style={styles.menuContainer}>
-          <View style={styles.menu}>
-                <Text>menu</Text>
-                
-          </View>
-          <View style={styles.menu}>
-                <Text>menu</Text>
-                
-          </View>
-        </View>
-        <View style={styles.menuContainer}>
-          <View style={styles.menu}>
-                <Text>menu</Text>
-                
-          </View>
-          <View style={styles.menu}>
-                <Text>menu</Text>
-                
-          </View>
-        </View>
-        <View style={styles.menuContainer}>
-          <View style={styles.menu}>
-                <Text>menu</Text>
-                
-          </View>
-          <View style={styles.menu}>
-                <Text>menu</Text>
-                
-          </View>
+        <View>
+          <Text>{menu.price}원</Text>
         </View>
       </View>
-      <View style={styles.order}>
+      );
+    }
+  }
+  printMenus2(menu){
+    if(menu.id%2==0) {
+    return (
+      <View style={styles.menu}>
         <View>
-          <Text style={{fontSize:20}}>menu1</Text>
-          <Text style={{fontSize:20}}>1개 20000원</Text>
+          <Text>{menu.menu}</Text>
         </View>
         <View>
-          <Text style={{fontSize:20}}>menu1</Text>
-          <Text style={{fontSize:20}}>1개 20000원</Text>
-        </View>
-        <View>
-          <Text style={{fontSize:20}}>menu1</Text>
-          <Text style={{fontSize:20}}>1개 20000원</Text>
-        </View>
-        <View>
-          <Text style={{fontSize:20}}>menu1</Text>
-          <Text style={{fontSize:20}}>1개 20000원</Text>
-        </View>
-        <View>
-          <Text style={{fontSize:20}}>menu1</Text>
-          <Text style={{fontSize:20}}>1개 20000원</Text>
-        </View>
-        <View>
-          <Text style={{fontSize:20}}>menu1</Text>
-          <Text style={{fontSize:20}}>1개 20000원</Text>
-        </View>
-        <View style={styles.orderButtonContainer}>
-          <Button title="버튼"/>
+          <Text>{menu.price}원</Text>
         </View>
       </View>
-    </ScrollView>
-  );
-};
-
+          );
+    }
+    else{
+      return;
+    }
+  }
+  render() {
+    return(
+      <ScrollView stickyHeaderIndices={[1]} style={{flex: 1, backgroundColor: 'rgb(242, 242, 247)'}}>
+        <View>
+          <View style={styles.menuContainer}>
+            {menus.map(menu=>(
+              this.printMenus(menu)
+            ))}
+          </View>
+          <View style={styles.menuContainer}>
+            {menus.map(menu=>(
+              this.printMenus2(menu)
+            ))}
+          </View>
+        </View>
+        <View style={styles.order}>
+          {cart.map(cart=>(
+            <View style={{margin:10}}>
+              <Text style={{fontSize:30}}>{cart.menu}</Text>
+              <Text style={{fontSize:20, textAlign:'right'}}>{cart.qty}개 - {cart.qty*cart.price}원</Text>
+            </View>
+          ))}
+          <View style={styles.orderButtonContainer}>
+            <Text style={{fontSize:30, textAlign:'right', margin:10}}>60000원</Text>
+            <Button title="주문하기"/>
+          </View>
+        </View>
+      </ScrollView>
+    );
+  }
+}
+const chartHeight = Dimensions.get('window').height;
 const styles = StyleSheet.create({
   order:{
     backgroundColor: 'white',
-    height: '62%',
+    height: chartHeight,
     width: '35%',
     top: 0,
     marginTop:0, 
@@ -97,12 +122,17 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     margin:10,
     borderRadius: 5,
-    width: '50%',
+    width: '47%',
     height:300
   },
   menuContainer:{
-    flexDirection:'row',flex: 2, justifyContent: 'center', alignItems: 'center', width:'60%',
-    marginLeft:20
+    flexDirection:'row',
+    flex: 2,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    width:'61%',
+    marginLeft:15,
+    overflow: 'scroll'
   },
   orderButtonContainer:{
     marginTop:'auto',
