981b953ce0be1d0ae58835349ffddeff31855903
jebuk97 <jebuk97@gmail.com>
2021-03-30 14:03:47 +0900

    장바구니 삭제, 중복 시 개수 추가

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -56,8 +56,7 @@ var categorys = [
   '카테고리1', '카테고리2'
 ]
 
-var cart1 = []
-var cart2 = []
+var sumPrice = 0;
 
 class Home extends React.Component{
   constructor(props){
@@ -66,16 +65,7 @@ class Home extends React.Component{
   }
 
   state = { category: 0,
-    cart: [{
-      menu: '돈까스',
-      qty: 2,
-      price: 10000
-    },
-    {
-      menu: '치즈돈까스',
-      qty: 2,
-      price: 12000
-    }],
+    cart: [],
   };
   printMenus(menu){
     if(menu.id%2==0) {
@@ -111,15 +101,25 @@ class Home extends React.Component{
       return;
     }
   }
+
   onPressMenu(menu, price){
     var tempCart = this.state.cart;
+    this.setState({cart:this.isDup(tempCart, menu, price)})
+  }
 
-    tempCart.push({
-    menu: menu,
-    qty: 1,
-    price: price
-    });
-    this.setState({cart:tempCart})
+  isDup(cart, menu, price){
+    for(var i=0;i<cart.length;i++){
+      if(cart[i].menu == menu){
+        cart[i].qty += 1;
+        return cart;
+      }
+    }
+    cart.push({
+      menu: menu,
+      qty: 1,
+      price: price
+      });
+    return cart;
   }
 
   onPressCat(index){
@@ -137,6 +137,29 @@ class Home extends React.Component{
     return temp;
   }
   
+  sumPriceHandler(){
+    var cart = this.state.cart;
+    var sum = 0;
+    for(var i=0;i<cart.length;i++){
+      sum += cart[i].price * cart[i].qty;
+    }
+    return sum;
+  }
+  onPressDel(menu){
+    this.popMenu(menu);
+  }
+  popMenu(menu){
+    console.log(menu);
+    var cart = this.state.cart;
+    var cart2 = [];
+    for(var i=0;i<cart.length;i++){
+      if(cart[i].menu == menu){
+        continue;
+      }
+      cart2.push(cart[i]);
+    }
+    this.setState({cart:cart2});
+  }
   render() {
     return(
       <View style={{flex:1}}>
@@ -169,11 +192,12 @@ class Home extends React.Component{
               {this.state.cart.map(cart=>(
                 <View style={{margin:10}}>
                   <Text style={{fontSize:30}}>{cart.menu}</Text>
-                  <Text style={{fontSize:20, textAlign:'right'}}>{cart.qty}개 - {cart.qty*cart.price}원</Text>
+                  <TouchableOpacity onPress={()=>this.onPressDel(cart.menu)}><Text style={{fontSize:20, color:'red'}}>X</Text></TouchableOpacity>
+                  <Text style={{fontSize:20, textAlign:'right'}}><TouchableOpacity><Text>-</Text></TouchableOpacity> {cart.qty}개 <TouchableOpacity><Text>+</Text></TouchableOpacity> {cart.qty*cart.price}원</Text>
                 </View>
               ))}
               <View style={styles.orderButtonContainer}>
-                <Text style={{fontSize:30, textAlign:'right', margin:10}}>60000원</Text>
+                <Text style={{fontSize:30, textAlign:'right', margin:10}}>{this.sumPriceHandler()}원</Text>
                 <Button title="주문하기"/>
               </View>
             </ScrollView>
